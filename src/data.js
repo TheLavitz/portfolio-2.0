a3e635039852719cac0ab3badf757aebe3f50d2f
TheLavitz <lavitz@gmail.com>
2024-02-02 08:32:11 -0800

    initial commit

---
+++ src/data.js
@@ -0,0 +1,52 @@
+export const projects = [
+  {
+    title: "Treasure Hunt",
+    subtitle: "React",
+    description:
+      "I revisted the first project I built with JavaScript and completely rebuilt it in React with a focus on dynamic styling",
+    image: "./treasure-hunt.png",
+    link: "https://corey-cass-treasurehunt.onrender.com/",
+  },
+  {
+    title: "Cat Tinder",
+    subtitle: "React and Rails",
+    description:
+      "A React applications with a Rails backend using a PostgreSQL database. Our focus here was to learn about handling CRUD functionality.",
+    image: "./cat-tinder.png",
+    link: "https://github.com/learn-academy-2023-golf/cat-tinder-backend-cc-em-cat-tinder",
+  },
+  {
+    title: "Text Based Game",
+    subtitle: "Ruby",
+    description:
+      "A Ruby application that handles text input and leads the user through a series of options generating outcomes based on choices the user makes.",
+    image: "./text-based-game.png",
+    link: "https://github.com/learn-academy-2023-golf/text-based-game-ces",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/1.jpg",
+    name: "Jesse Hicks",
+    company: "Zoozle",
+  },
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/94.jpg",
+    name: "Ruben Alvarez",
+    company: "Dooble.io",
+  },
+];
+
+export const skills = [
+  "Ruby on Rails",
+  "Ruby",
+  "React",
+  "JavaScript",
+  "Jest",
+  "PostgreSQL",
+];
